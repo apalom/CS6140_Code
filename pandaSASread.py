424fb34c227fa110b90ac2621eef76ff5de76c87
apalom <35828580+apalom@users.noreply.github.com>
2018-02-19 09:45:43 -0700

    Initial commit

---
+++ pandaSASread.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+import pandas
+
+pandas.read_sas(r"C:\Users\Alex Palomino\Documents\PythonScripts\SAS\dayv2pub.sas7bdat",format='sas7bdat')
---
+++ pandasCSVread.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Feb 18 22:57:53 2018
+
+@author: Alex Palomino
+"""
+
+import pandas as pd
+
+df = pd.read_csv(r'C:\Users\Alex Palomino\Google Drive\Classes\18_Spring\CS6140 Data Mining\CS6140 Project\Data\CSV\DAYV2PUB.CSV', header=0)
+df.head() # shows first 5 rows
+df.tail() # shows last 5 rows
+list(df) # shows all column headers
+df['TRIPPURP'].describe()
+df.shape
