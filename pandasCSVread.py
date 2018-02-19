57e4b4c811a0240703115b1d5117e98346567f05
apalom <35828580+apalom@users.noreply.github.com>
2018-02-19 10:33:50 -0700

    Added Timing Functions

--- pandasCSVread.py
+++ pandasCSVread.py
@@ -6,11 +6,22 @@ Created on Sun Feb 18 22:57:53 2018
 """
 
 import pandas as pd
+import timeit
+
+timeExec= 0
+start_time = timeit.default_timer()
 
 df = pd.read_csv(r'C:\Users\Alex Palomino\Google Drive\Classes\18_Spring\CS6140 Data Mining\CS6140 Project\Data\CSV\DAYV2PUB.CSV', header=0)
-df.head() # shows first 5 rows
+colNames = list(df) # shows all column headers
+first5rows = df.head() # shows first 5 rows
 df.tail() # shows last 5 rows
-list(df) # shows all column headers
+
 df['TRIPPURP'].describe()
 
-print(df.shape)
+print('Dataframe Shape:', df.shape)
+
+elapsed = timeit.default_timer() - start_time
+timeExec.append(elapsed)
+
+# timeit statement
+print('Execution time:', timeExec)
