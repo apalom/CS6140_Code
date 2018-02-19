9571e9a0f4b406b2c316b2d934592dd37136d54f
apalom <35828580+apalom@users.noreply.github.com>
2018-02-19 13:03:48 -0700

    Created Filtered DF ver1

--- pandasCSVread.py
+++ pandasCSVread.py
@@ -10,14 +10,27 @@ import timeit
 
 start_time = timeit.default_timer()
 
-df = pd.read_csv(r'C:\Users\Alex Palomino\Google Drive\Classes\18_Spring\CS6140 Data Mining\CS6140 Project\Data\CSV\DAYV2PUB.CSV', header=0)
-colNames = list(df) # shows all column headers
-first5rows = df.head() # shows first 5 rows
-df.tail() # shows last 5 rows
+df0 = pd.read_csv(r'C:\Users\Alex Palomino\Google Drive\Classes\18_Spring\CS6140 Data Mining\CS6140 Project\Data\CSV\DAYV2PUB.CSV', header=0)
 
-df['TRIPPURP'].describe()
+df1 = df0.filter(['TDCASEID','TRAVDAY','STRTTIME','DWELTIME','ENDTIME','TRIPPURP',
+                  'WHYFROM','WHYTO','WHYTRP1S','WHYTRP90','WHODROVE',
+                  'CENSUS_D','CENSUS_R','DRIVER','AWAYHOME','FRSTHM','TDTRPNUM',
+                  'TDWKND','TRPACCMP','TRPHHACC','TRVLCMIN','TRVL_MIN','TRWAITTM',
+                  'VEHTYPE','VEHYEAR','VMT_MILE','HHFAMINC','HHSIZE','HHSTATE','HOMEOWN',
+                  'NUMADLT','NUMONTRIP','PRMACT','PAYPROF','PROXY','PRMACT','R_AGE','R_SEX'], axis=1)
 
-print('Dataframe Shape:', df.shape)
+
+colNames0 = list(df0) # shows all column headers
+colNames1 = list(df1) # shows all column headers
+first5rows0 = df0.head() # shows first 5 rows
+first5rows1 = df1.head() # shows first 5 rows
+last5rows0 = df0.tail() # shows last 5 rows
+last5rows1 = df1.tail() # shows last 5 rows
+
+df0['TRIPPURP'].describe()
+
+print('Dataframe Raw Shape:', df0.shape)
+print('Dataframe Filtered Shape:', df1.shape)
 
 elapsed = timeit.default_timer() - start_time
 
