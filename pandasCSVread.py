71c6f870314b284089fe1aaa25eef119e6eddda4
apalom <35828580+apalom@users.noreply.github.com>
2018-02-20 14:05:12 -0700

    Added Comments

--- pandasCSVread.py
+++ pandasCSVread.py
@@ -5,17 +5,24 @@ Created on Sun Feb 18 22:57:53 2018
 @author: Alex Palomino
 """
 
+# import libraries
 import pandas as pd
 import timeit
 import matplotlib.pyplot as plt
 
+# initialize values
 start_time = timeit.default_timer()
-
 whyID = {}
 whyIDsum = {}
 
-df0 = pd.read_csv(r'C:\Users\Alex Palomino\Google Drive\Classes\18_Spring\CS6140 Data Mining\CS6140 Project\Data\CSV\DAYV2PUB.CSV', header=0)
 
+# NHTS2009 Data Location for Alex's Laptop 
+# df0 = pd.read_csv(r'C:\Users\Alex Palomino\Google Drive\Classes\18_Spring\CS6140 Data Mining\CS6140 Project\Data\CSV\DAYV2PUB.CSV', header=0)
+
+# NHTS2009 Data Location for Alex's Lab Computer
+df0 = pd.read_csv(r'C:\Users\Alex\Google Drive\Classes\18_Spring\CS6140 Data Mining\CS6140 Project\Data\CSV\DAYV2PUB.CSV', header=0)
+
+# filter dataframe zero (raw NHTS2009) to columns listed in filter
 df1 = df0.filter(['TDCASEID','TRAVDAY','STRTTIME','DWELTIME','ENDTIME','TRIPPURP',
                   'WHYFROM','WHYTO','WHYTRP1S','WHYTRP90','WHODROVE',
                   'CENSUS_D','CENSUS_R','DRIVER','AWAYHOME','FRSTHM','TDTRPNUM',
@@ -23,11 +30,11 @@ df1 = df0.filter(['TDCASEID','TRAVDAY','STRTTIME','DWELTIME','ENDTIME','TRIPPURP
                   'VEHTYPE','VEHYEAR','VMT_MILE','HHFAMINC','HHSIZE','HHSTATE','HOMEOWN',
                   'NUMADLT','NUMONTRIP','PRMACT','PAYPROF','PROXY','PRMACT','R_AGE','R_SEX'], axis=1)
 
-# function call
+# function call to attribute why descriptions with why codes
 from funcWhyID import funcWhyID
 [df1, whyID, whyIDsum] = funcWhyID(df1, whyID, whyIDsum)
 
-# build out DF table
+# build out dataframe table
 colNames0 = list(df0) # shows all column headers
 colNames1 = list(df1) # shows all column headers
 first5rows0 = df0.head() # shows first 5 rows
@@ -37,6 +44,7 @@ last5rows1 = df1.tail() # shows last 5 rows
 
 df0['TRIPPURP'].describe()
 
+# print data shapes (rows x columns)
 print('Dataframe Raw Shape:', df0.shape)
 print('Dataframe Filtered Shape:', df1.shape)
 
@@ -45,9 +53,9 @@ elapsed = timeit.default_timer() - start_time
 # timeit statement
 print('Execution time: {0:.4f} sec'.format(elapsed))
 
-# %% plotting
+# %% plotting section
 
-# Plots histogram
+# plots histogram
 df1['WHYTRP1S'].hist(bins=25)
 
 #plt.plot("whyDescSmry",type="bar")
